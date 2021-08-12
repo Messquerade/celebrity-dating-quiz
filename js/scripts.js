75cd90965e9d47241fb6293bead9267b002af89f
Messquerade <annac.klingberg@gmail.com>
2021-08-12 13:31:05 -0700

    add results and name input
    Co-authored-by: Benjamin Wilson <benjaminw1030@gmail.com>
    Co-authored-by: Mark Chang <Marksrc7@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,36 @@
 $(document).ready(function() {
   $("#quiz").submit(function(event) {
     event.preventDefault();
+    const name = $("#name").val();
     const Input1 = parseInt($("input:radio[name=pet]:checked").val());
     const Input2 = parseInt($("input:radio[name=drink]:checked").val());
     const Input3 = parseInt($("input:radio[name=hobby]:checked").val());
     const Input4 = parseInt($("input:radio[name=place]:checked").val());
     const Input5 = parseInt($("input:radio[name=season]:checked").val());
 
-    // const nameInput = $("input#name").val();
-    // const seasonInput = $("#season").val();
-    // const petInput = $("input:radio[name=pet]:checked").val();
-    // const DOBInput = $("#DOB").val();
-    // const shirtInput = $("#shirt").val();
+    const total = Input1 + Input2 + Input3 + Input4 + Input5
+    $("#inputName").text(name);
+    if (!name) {
+      $("#nameAlert").show();
+    } else if (total >= 12) {
+      $("#result").show();
+      $("#gandalf").show();
+      $("#aragorn").hide();
+      $("#frodo").hide();
+      $("nameAlert").hide();
+    } else if (total < 10 && total > 7) {
+      $("#result").show();
+      $("#aragorn").show();
+      $("#gandalf").hide();
+      $("#frodo").hide();
+      $("nameAlert").hide();
+    } else {
+      $("#result").show();
+      $("#gandalf").hide();
+      $("#aragorn").hide();
+      $("#frodo").show();
+      $("#nameAlert").hide();
+    }
 
-    // $(".name").text(nameInput);
-    // $(".season").text(seasonInput);
-    // $(".pet").text(petInput);
-    // $(".DOB").text(DOBInput);
-    // $(".shirt").text("this color").css("color", shirtInput);
-   
-    // $("#response").show();
-  })
-})
+  });
+});
